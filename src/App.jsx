7fed75f90b95a484ee18b64cd6a251136d2a23ae
To'raqulov Asfandiyor <asfan.webdeloper@gmail.com>
2025-03-22 09:07:55 +0500

    Yangi o'zgartirishlar

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import React from "react";
+import { HashRouter as Router } from "react-router-dom";
 import { Header, HomePage, Contact, Booking, Polise, FiveDaysTour, SixDaysTour, TenDaysTour, Footer, Footer2, BizHaqimizda, IndividualTurs, Sayohatlar, Admin, License } from "./components/index";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <main style={{ marginTop: "55px" }}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/booking" element={<Booking />} />
-          <Route path="/polise" element={<Polise/>}/>   
-          <Route path="/5-days-tour" element={<FiveDaysTour/>}/>
-          <Route path="/6-days-tour" element={<SixDaysTour/>}/>
-          <Route path="/10-days-tour" element={<TenDaysTour/>}/>
-          <Route path="/our-mission" element={<BizHaqimizda/>}/>
-          <Route path="/individual-tours" element={<IndividualTurs/>}/>
-          <Route path="/tours" element={<Sayohatlar/>}/>
-        <Route path="/admin" element={<Admin/>}/>
-          <Route path="/license" element={<License/>}/>
-        </Routes>
-      </main>
-     <Footer2/>
-    </div>
+    <Router>
+      <div> 
+        <Header />
+        <main style={{ marginTop: "55px" }}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route path="/polise" element={<Polise/>}/>   
+            <Route path="/5-days-tour" element={<FiveDaysTour/>}/>
+            <Route path="/6-days-tour" element={<SixDaysTour/>}/>
+            <Route path="/10-days-tour" element={<TenDaysTour/>}/>
+            <Route path="/our-mission" element={<BizHaqimizda/>}/>
+            <Route path="/individual-tours" element={<IndividualTurs/>}/>
+            <Route path="/tours" element={<Sayohatlar/>}/>
+            <Route path="/admin" element={<Admin/>}/>
+            <Route path="/license" element={<License/>}/>
+          </Routes>
+        </main>
+        <Footer2/>
+      </div>
+    </Router>
   );
 }
 
